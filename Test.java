b3b16d350d78ddd600a8e88dd7f7a4aafee0ed8c
eunji.Baek <qordmswl1541@naver.com>
2020-05-06 13:00:00 +0900

    hello 제거

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		system.out.println("Hello Git~");
+		system.out.println("git");
 		system.out.println("Hi Git!");
 		system.out.println("branch1");
 		system.out.println("branch2");
