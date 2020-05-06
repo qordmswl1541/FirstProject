2b6d55fb1537a4130f960c8307d0ea983537dc58
eunji.Baek <qordmswl1541@naver.com>
2020-05-06 15:02:00 +0900

    test.java 첫줄주석

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		system.out.println("Hello ");
+//		system.out.println("Hello ");
 		system.out.println("git");
 		system.out.println("Hi Git!");
 		system.out.println("branch1");
